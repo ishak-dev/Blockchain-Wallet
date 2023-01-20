8b7ef8613ac40ae55a7c1234c3a2be35cd6e390d
Ishak Kazić <ishak.kazic@gmail.com>
2023-01-20 19:27:42 +0100

    deploy contract

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-goerli.g.alchemy.com/v2/lrk1hEyGxyNs3DyQiEXta42zn5eE_GgN
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.17",
-};
+require('@nomiclabs/hardhat-waffle')
+
+
+module.exports ={
+  solidity:"0.8.0",
+  networks:{
+    goerli:{
+      url:"https://eth-goerli.g.alchemy.com/v2/lrk1hEyGxyNs3DyQiEXta42zn5eE_GgN",
+      accounts: ['0beb3bec31462e2aa93e6a548db3449ae50bca23c678fc5b637cfe6a170af3bf']
+    }
+  }
+}
